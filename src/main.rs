2294e3e6065b298e23b3ae8c80b262a29fc6431c
Toni Guli <pllumiguli@icloud.com>
2021-09-24 22:29:19 +0200

    added argument parsing function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env::args;
+
+fn parse_argument() -> Option<u32> {
+	let args = args().collect::<Vec<String>>();
+	
+	if let Some(a) = args.get(1) {
+		Some(a.parse::<u32>().expect("Invalid argument entered, insert number"))
+	} else {
+		None
+	}
+}
+
 fn main() {
 	println!("Hello, world!");
+	println!("arg: {:?}", parse_argument());
 }
