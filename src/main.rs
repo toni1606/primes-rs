c465ac4da729e6fbb6544794e85426e5d69d1b6e
Toni Guli <pllumiguli@icloud.com>
2021-09-24 22:53:37 +0200

    created naive prime_sieve() to return a vector of prime numbers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env::args;
+use std::num;
+use std::thread;
 
 fn parse_argument() -> Option<u32> {
 	let args = args().collect::<Vec<String>>();
@@ -10,7 +12,25 @@ fn parse_argument() -> Option<u32> {
 	}
 }
 
+fn prime_sieve(max_num: u32) -> Vec<u32> {
+	let mut out: Vec<u32> = Vec::new();
+	for i in 1..max_num {
+		let mut is_prime: bool = true;
+		for  j in 2..((max_num as f64).sqrt() + 1.0) as u32 {
+			if i % j == 0 {
+				is_prime = false;
+			}
+		}
+
+		if is_prime {
+			out.push(i);
+		}
+	}
+
+	out
+}
+
 fn main() {
 	println!("Hello, world!");
-	println!("arg: {:?}", parse_argument());
+	println!("primes: {:?}", prime_sieve(parse_argument().unwrap()));
 }
