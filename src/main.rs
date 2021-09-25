82c6c8265ba3f81a1e169d6e9949ebc4f139cd56
Toni Guli <pllumiguli@icloud.com>
2021-09-25 13:42:35 +0200

    bug: limit was the same for every iteration -> many numbers were not being tested

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::thread;
 
 fn parse_argument() -> Option<u32> {
 	let args = args().collect::<Vec<String>>();
-	
+
 	if let Some(a) = args.get(1) {
 		Some(a.parse::<u32>().expect("Invalid argument entered, insert number"))
 	} else {
@@ -13,22 +13,30 @@ fn parse_argument() -> Option<u32> {
 
 fn start_threads(thread_count: u8, max_num: u32) -> Vec<Vec<u32>> {
 	let numbers_per_thread = max_num / thread_count as u32;
-	let test_limit = (max_num as f64).sqrt() as u32;
-
+	
 	let mut threads = Vec::with_capacity(thread_count as usize);
 	for i in 0..thread_count {
 		let min = 1 + (numbers_per_thread * i as u32);
 		let max = min + numbers_per_thread;
-
+		
 		threads.push(thread::spawn( move || {
 			let mut out: Vec<u32> = Vec::new();
 			
 			for i in min..max {
+				let test_limit = (i as f64).sqrt() as u32;
 				let mut is_prime: bool = true;
-				
-				for j in 2..test_limit + 1 {
+
+				if i == 2 {
+					out.push(i);
+					continue;
+				} else if i % 2 == 0 {
+					continue;
+				}
+
+				for j in 3..test_limit {
 					if i % j == 0 {
 						is_prime = false;
+						break;
 					}
 				}
 
@@ -51,6 +59,5 @@ fn start_threads(thread_count: u8, max_num: u32) -> Vec<Vec<u32>> {
 }
 
 fn main() {
-	start_threads(8, parse_argument().expect("Invalid number entered"));
-	println!("Hello, world!");
+	println!("{:?}", start_threads(8, parse_argument().expect("Invalid number entered")));
 }
